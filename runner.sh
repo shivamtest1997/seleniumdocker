bfbb348360f301f0bec983bb7ce5643684a61fba
shivamTest <shivamest1997@gmail.com>
2024-01-30 23:15:00 +0530

    added a

--- runner.sh
+++ runner.sh
@@ -15,7 +15,7 @@ echo "TEST_SUITE    : ${TEST_SUITE}"
 echo "-------------------------------------------"
 # Do not start the tests immediately. Hub has to be ready with browser nodes
 echo "Checking if hub is ready..!"
-#count=0
+count=0
 while [ "$( curl -s http://${HUB_HOST:-hub}:4444/status | jq -r .value.ready )" != "true" ]
 do
   count=$((count+1))
@@ -25,7 +25,7 @@ do
       echo "**** HUB IS NOT READY WITHIN 30 SECONDS ****"
       exit 1
   fi
-#  sleep 1
+  sleep 1
 done
 # At this point, selenium grid should be up!
 echo "Selenium Grid is up and running. Running the test...."
