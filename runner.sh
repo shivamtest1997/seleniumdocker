85b121aa44f2142951e5438bfc2b4724061acd97
shivamTest <shivamest1997@gmail.com>
2024-01-30 23:04:29 +0530

    added a

--- runner.sh
+++ runner.sh
@@ -18,18 +18,18 @@ echo "-------------------------------------------"
 
 # Do not start the tests immediately. Hub has to be ready with browser nodes
 echo "Checking if hub is ready..!"
-count=0
-while [ "$( curl -s http://${HUB_HOST:-hub}:4444/status | jq -r .value.ready )" != "true" ]
-do
-  count=$((count+1))
-  echo "Attempt: ${count}"
-  if [ "$count" -ge 30 ]
-  then
-      echo "**** HUB IS NOT READY WITHIN 30 SECONDS ****"
-      exit 1
-  fi
-  sleep 1
-done
+#count=0
+#while [ "$( curl -s http://${HUB_HOST:-hub}:4444/status | jq -r .value.ready )" != "true" ]
+#do
+#  count=$((count+1))
+#  echo "Attempt: ${count}"
+#  if [ "$count" -ge 30 ]
+#  then
+#      echo "**** HUB IS NOT READY WITHIN 30 SECONDS ****"
+#      exit 1
+#  fi
+#  sleep 1
+#done
 
 # At this point, selenium grid should be up!
 echo "Selenium Grid is up and running. Running the test...."
@@ -42,4 +42,3 @@ java -cp 'libs/*' \
      org.testng.TestNG \
      -threadcount "${THREAD_COUNT:-1}" \
      test-suites/"vendorPortal.xml"
-
